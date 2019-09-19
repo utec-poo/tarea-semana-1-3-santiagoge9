27d3300289cf5db5a7e88fd0204f0e2fd8b657e8
santiagoge9 <40152993+santiagoge9@users.noreply.github.com>
2019-09-19 13:17:02 -0500

    Update main.cpp

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
-#include "Tipos.h"
-using namespace std;
+using namespace std ;
+using entero = int ;
 
 int main() {
+    entero numero = 0 , valor = 0 , iterativo = 0 ;
+    entero ContadorD = 0 , ContadorA = 0 ,  ContadorB = 0 ;
+    bool flag = false;
+    //control de entrada
+    do {
+        cin >> numero;
+        if (numero >= 100)
+            flag = true;
+    } while (!flag);
+    //  " !flag "  -->  " flag == false "
 
-entero numero;
+    // LECTURA DEL NUMERO
+    // 'El valor se inicializa en el numero'
+    valor = numero ;
+    // 'El programa trabaja leyendo el iterativo como los dígitos ordenados del numero'
+    do{
+        cout << "\n    Listo    " << valor ;
 
+        iterativo = valor % 10 ;
+        // 'El iterativo será el último dígito del número'
+        if ((iterativo % 2) == 0)
+        {   ContadorD += 1 ;
+            ContadorA += 1 ;
+        }
+        if ( iterativo % 2 != 0 )
+        {   ContadorD += 1 ;
+            ContadorB += 1 ;
+        }
+        //se genera un nuevo valor para trabajar
+        valor = valor / 10 ;
+        cout << "\n    Listo    " << valor ;
 
-cout << "ingrese un numero:";
+    } while ( valor > 0 ) ;
 
-cin >> numero;
+    cout << "\n    El numero es: " << numero ;
+    cout << "\n    El numero de dígitos es: " << ContadorD ;
+    cout << "\n    El numero de dígitos pares es :" << ContadorA ;
+    cout << "\n    El numero de dígitos impares es :" << ContadorB ;
 
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    return 0;
 }
