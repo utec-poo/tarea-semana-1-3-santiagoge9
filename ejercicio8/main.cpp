bf2dfb0401a6ff3f2de2a617c0ba0b5ffe7ea619
santiagoge9 <40152993+santiagoge9@users.noreply.github.com>
2019-09-19 19:50:43 -0500

    Update main.cpp

--- ejercicio8/main.cpp
+++ ejercicio8/main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
-#include "Tipos.h"
-using namespace std;
+using namespace std ;
+using entero = int ;
 
 int main() {
+    entero numero = 0 ;
+    //  CONTROL DE ENTRADA DEL TAMAÑO DE LA MATRIZ
+    do{
+        cin >> numero ;
+    } while ( numero < 1 || numero > 30 ) ;
 
-entero numero;
+    //  SE CREA UNA MATRIZ DINAMICA
+    char Matriz[numero][numero] ;
 
-cout << "\ningrese el numero:";
-cin >> numero;
+    //  SE RELLENA LA MATRIZ CON EL PATRON SOLICITADO
+    for ( entero i = 0 ; i < numero ; i++ ) {
+        for ( entero j = 0 ; j < numero ; j++ ) {
+            if ( i > j || i == j )
+                Matriz[i][j] = '*' ;
+            else
+                Matriz[i][j] = ' ' ;
+        }
+    }
 
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    //  SE IMPRIME LA MATRIZ
+    for ( entero i = 0 ; i < numero ; i++ ) {
+        cout << "\n" ;
+        for ( entero j = 0 ; j < numero ; j++ ) {
+            cout << Matriz[i][j] << ' ' ;
+        }
+    }
+    cout << "\n\n" ;
+    return 0;
 }
